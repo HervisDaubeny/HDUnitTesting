8e92111fdb0851f709440b83351e6e07285141df
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-20 23:55:05 +0200

    Will not support generic classes.

--- CustomersProject/Greeter.cs
+++ CustomersProject/Greeter.cs
@@ -4,14 +4,10 @@ using System.Text;
 
 namespace CustomersNamespace {
 
-    public class GenericGreeter<T, U> {
+    public class Greeter {
 
-        public T TProp { get; set; }
-        public U UProp { get; set; }
+        public Greeter() {
 
-        public GenericGreeter(T TProp, U UProp) {
-            this.TProp = TProp;
-            this.UProp = UProp;
         }
 
         public void Greet <V, W> (V Vparam, W Wparam, int num) {
