6110eb81d6b0da30acda094800157d8673756bab
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-21 00:05:49 +0200

    Created class for serializing and deserializing results.

---
+++ HDUnitDev/HDUnitLibrary/HDTestResultSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDUnit {
+    public static class HDTestResultSerializer {
+
+        private static string Path = null;
+
+        public static void SetPath(string Path) {
+            HDTestResultSerializer.Path = Path;
+        }
+
+        public static void Serialize(TestResultContainer[] TestResults) {
+
+        }
+
+        public static TestResultContainer[] Deserialize() {
+
+            return null; //TODO: proper return
+        }
+    }
+}
