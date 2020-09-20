c19555c5e87a8edd4011ef8a194210c941a1d733
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-20 23:58:51 +0200

    Added exception when parsing input and running dependent methods without dependency.

---
+++ HDUnitDev/HDUnitLibrary/Exceptions/HDParseInputException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDUnit.Exceptions {
+
+    public class HDParseInputException : HDRootException {
+
+        public HDParseInputException() { }
+
+        public HDParseInputException(string Message) :base(Message) { }
+
+        public HDParseInputException(string Message, Exception Inner) :base(Message, Inner) { }
+    }
+}
---
+++ HDUnitDev/HDUnitLibrary/Exceptions/HDRunAfterMethodException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDUnit.Exceptions {
+    public class HDRunAfterMethodException : HDRootException {
+        public HDRunAfterMethodException() { }
+
+        public HDRunAfterMethodException(string Message) : base(Message) { }
+
+        public HDRunAfterMethodException(string Message, Exception Inner) : base(Message, Inner) { }
+    }
+}
