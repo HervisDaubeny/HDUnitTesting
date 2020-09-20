75948f9f386fce3993f1b833a7a8f2ddc3b53e96
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-21 00:04:51 +0200

    Created class for printing test results.

---
+++ HDUnitDev/HDUnitLibrary/HDResultPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDUnit {
+    public static class HDResultPrinter {
+        private static string separator = "==========================================";
+
+        public static void Print(TestResultContainer[] TestResults) {
+            Console.WriteLine(separator);
+            Console.WriteLine("TEST RESULTS");
+            Console.WriteLine(separator);
+            foreach (var result in TestResults) {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
