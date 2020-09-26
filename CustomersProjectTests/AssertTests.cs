b8e45de1c519ac2e16985b0768d7b65e277a8dbd
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-26 17:53:59 +0200

    Added test classes.

---
+++ CustomersProjectTests/AssertTests.cs
@@ -0,0 +1,42 @@
+﻿using HDUnit.Assert;
+using HDUnit.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomersProjectTests {
+
+    [HDTestClass]
+    [HDClassConstructor("for testing Assert class, by paranoid customer")]
+    public class AssertTests {
+
+        public string Purpouse { get; set; }
+
+        public AssertTests(string Purpouse) {
+            this.Purpouse = Purpouse;
+        }
+
+        [HDTestMethod]
+        [HDParameters("paranoid customer")]
+        [HDParameters("nonexistent substring")]
+        public void StringContainsTest(string toTest) {
+            HDAssert.Contains(toTest, Purpouse);
+        }
+
+        [HDTestMethod]
+        [HDParameters(new int[] { 1,2,3,4,5,6,7,8,9})]
+        [HDParameters(new int[] { 1,3,5,7,9})]
+        public void AllInCollectionTest(int[] collection) {
+            HDAssert.All(collection, item => { var i = item / (item % 2); });
+        }
+
+        [HDTestMethod]
+        [HDParameters(new int[] { 1, 2, 3, 5, 6, 8, 9, 0 })]
+        [HDParameters(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        [HDParameters(new int[] { 9, 2, 3, 4, 5, 6, 7, 8, 1 })]
+        public static void CollectionsEqual(int[] testArray) {
+            int[] referenceArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            HDAssert.Equal<int>(referenceArray, testArray);
+        }
+    }
+}
---
+++ CustomersProjectTests/NamespaceTests.cs
@@ -0,0 +1,18 @@
+﻿using HDUnit.Assert;
+using HDUnit.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomersProjectTests.Nested {
+
+    [HDTestClass]
+    [HDClassConstructor()]
+    public class NamespaceTests {
+
+        [HDTestMethod]
+        public static void OriginalTestName() {
+            HDAssert.IsTrue(true);
+        }
+    }
+}
