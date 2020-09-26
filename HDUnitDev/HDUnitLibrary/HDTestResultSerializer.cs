9f58f3d77667f37af23f1d0c7e2a8781da804ad5
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-26 18:06:54 +0200

    Class for serializing and deserialization the test results.

--- HDUnitDev/HDUnitLibrary/HDTestResultSerializer.cs
+++ HDUnitDev/HDUnitLibrary/HDTestResultSerializer.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using HDUnit.Exceptions;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace HDUnit {
     public static class HDTestResultSerializer {
 
-        private static string Path = null;
-
+        private static string Path = Directory.GetCurrentDirectory() + "\\testResult.json";
         public static void SetPath(string Path) {
             HDTestResultSerializer.Path = Path;
         }
 
         public static void Serialize(TestResultContainer[] TestResults) {
-
+            try {
+                string serialization = JsonConvert.SerializeObject(TestResults, Formatting.Indented);
+                File.WriteAllText(Path, serialization);
+            }
+            catch (JsonWriterException jwex) {
+                throw new HDSerializationException("Can't serialize data.", jwex);
+            }
+            catch (IOException ioex) {
+                throw new HDSerializationException("Writing to a file failed.", ioex);
+            }
+            catch (Exception ex) {
+                throw new HDSerializationException("Unexpected error during serialization.", ex);
+            }
         }
 
         public static TestResultContainer[] Deserialize() {
+            TestResultContainer[] lastRunResults = null;
+            try {
+                string json = File.ReadAllText(Path);
+                lastRunResults = JsonConvert.DeserializeObject<TestResultContainer[]>(json);
+            }
+            catch (IOException ioex) {
+                throw new HDSerializationException("Reading file has failed.", ioex);
+            }
+            if (lastRunResults.Length < 1) {
+                throw new HDSerializationException("Provided file could not be deserialized.");
+            }
 
-            return null; //TODO: proper return
+            return lastRunResults;
         }
     }
 }
