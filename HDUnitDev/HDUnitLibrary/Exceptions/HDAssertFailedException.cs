7ea8e888eca87f505547bd9a49f9dc5659594b69
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-26 18:00:23 +0200

    Documented core exception.

--- HDUnitDev/HDUnitLibrary/Exceptions/HDAssertFailedException.cs
+++ HDUnitDev/HDUnitLibrary/Exceptions/HDAssertFailedException.cs
@@ -5,12 +5,47 @@ using System.Threading.Tasks.Sources;
 
 namespace HDUnit.Exceptions {
 
+    /// <summary>
+    /// Exception thrown when HDAssert method fails.
+    /// </summary>
     public class HDAssertFailedException : HDRootException {
 
-        public HDAssertFailedException() { }
+        /// <summary>
+        /// Error dictionary for extended collection of wrong data.
+        /// </summary>
+        public IDictionary<string, string[]> Errors { get; set; }
 
-        public HDAssertFailedException(string Message) :base(Message) { }
+        /// <summary>
+        /// Exception thrown when HDAssert method fails.
+        /// </summary>
+        public HDAssertFailedException() {
+            Errors = new Dictionary<string, string[]>();
+        }
 
-        public HDAssertFailedException(string Message, Exception Inner) :base(Message, Inner) { }
+        /// <summary>
+        /// Exception thrown when HDAssert method fails.
+        /// </summary>
+        /// <param name="Message">Exception message</param>
+        public HDAssertFailedException(string Message) : base(Message) {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        /// <summary>
+        /// Exception thrown when HDAssert method fails.
+        /// </summary>
+        /// <param name="Message">Exception message</param>
+        /// <param name="Errors">Collection of errors encountered during test</param>
+        public HDAssertFailedException(string Message, IDictionary<string, string[]> Errors) : base(Message) {
+            this.Errors = Errors;
+        }
+
+        /// <summary>
+        /// Exception thrown when HDAssert method fails.
+        /// </summary>
+        /// <param name="Message">Exception message</param>
+        /// <param name="Inner">The exception that occurred during test</param>
+        public HDAssertFailedException(string Message, Exception Inner) : base(Message, Inner) {
+            Errors = new Dictionary<string, string[]>();
+        }
     }
 }
