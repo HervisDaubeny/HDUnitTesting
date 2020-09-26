b1fbd0fa1f344684f1055aaa338385d6282c090f
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-26 17:59:03 +0200

    Added exception to guard serialization of test results.

---
+++ HDUnitDev/HDUnitLibrary/Exceptions/HDSerializationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HDUnit.Exceptions {
+
+   public class HDSerializationException : HDRootException {
+
+        public HDSerializationException() { }
+
+        public HDSerializationException(string Message) : base(Message) { }
+
+        public HDSerializationException(string Message, Exception Inner) : base(Message, Inner) { }
+    }
+}
