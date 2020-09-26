b5bd5e5829c26d624db4f837f1372bfe039d7cba
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2020-09-26 17:53:41 +0200

    Cleaned TestRun.

--- CustomersProjectTests/TestRun.cs
+++ CustomersProjectTests/TestRun.cs
@@ -12,41 +12,7 @@ using HDUnit.Extensions;
 namespace CustomersProjectTests {
     public class TestRun {
         public static void Main(string[] args) {
-
             HDTester.InitTests();
-            //HDTester.RunTest("dummyName");
-            /*
-            // ### LEGACY ###
-            var classes = Assembly.GetExecutingAssembly().GetTypes();
-            foreach (var cl in classes) {
-                if (cl.GetCustomAttribute<HDTestClassAttribute>() is HDTestClassAttribute) {
-                    Console.WriteLine(cl.Name);
-                    if (cl.Name is nameof(GenericGreeterTests)) {
-                        var GenClass = new GenericGreeterTests();
-
-
-                        var meth = cl.GetMethods();
-                        foreach (var met in meth) {
-                            var attributes = met.GetCustomAttributes<HDRootAttribute>(inherit: true);
-                            foreach (var instance in attributes) {
-                                if (instance is HDGenericParametersAttribute attValue) {
-                                    MethodInfo myGenericMethod = met.MakeGenericMethod(attValue.Types);
-                                    var testName = myGenericMethod.GetName();
-                                    try {
-                                        myGenericMethod.Invoke(GenClass, attValue.Parameters);
-                                        Console.Write($"{testName}  |  ");
-                                        Console.WriteLine("Test passed.");
-                                    }
-                                    catch(TargetInvocationException E) when(E.InnerException is HDAssertFailedException inner) {
-                                        Console.Write($"{testName}  |  ");
-                                        Console.WriteLine(inner.Message);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } */
         }
     }
 }
